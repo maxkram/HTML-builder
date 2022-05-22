36228a9f35d7624c282170abca61085a46681622
maxkram <max.kramer@live.com>
2022-05-22 17:55:17 -0400

    add: add 5rd task (Sun, May 22, 2022 05:55:17 PM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,7 +3,31 @@ const path = require('path');
 
 async function makeBundle() {
   try {
-    console.log('\x1b[36m', 'bundle.css, вроде, создался?');
+    await fs.promises.writeFile(
+      path.join(__dirname, 'project-dist', 'bundle.css'),
+      ''
+    );
+    const styles = await fs.promises.readdir(path.join(__dirname, 'styles'), {
+      withFileTypes: true,
+    });
+
+    for (const style of styles) {
+      const filePath = path.join(path.join(__dirname, 'styles'), style.name);
+      const filePathObject = path.parse(filePath);
+      if (style.isFile() && filePathObject.ext === '.css') {
+        let data = '';
+        const readableStream = fs.createReadStream(filePath, 'utf-8');
+        readableStream.on('data', (chunk) => (data += chunk));
+        readableStream.on('end', () =>
+          fs.promises.appendFile(
+            path.join(__dirname, 'project-dist', 'bundle.css'),
+            data
+          )
+        );
+      }
+    }
+
+    console.log('\x1b[36m', 'Файлик bundle.css, вроде, готов?');
   } catch (err) {
     console.error(err);
   }
