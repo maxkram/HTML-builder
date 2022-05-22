4bb9ce4e9ed774ba227d09f22402abe5c253efd5
maxkram <max.kramer@live.com>
2022-05-21 22:09:27 -0400

    add: add 2nd task (Sat, May 21, 2022 10:09:27 PM)

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+let phrase = () => {
+  process.stdout.write('Напишите что-нибудь душевное: ');
+};
+
+let makeTxt = () => {
+  fs.writeFile(path.join(__dirname, '.', 'text.txt'), '', (err) => {
+    if (err) throw err;
+  });
+};
+
+makeTxt();
+
+process.stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    console.log('Как, уже уходите? Жаль...');
+    process.exit();
+  } else {
+    fs.appendFile(
+      path.join(__dirname, '.', 'text.txt'),
+      `${data.toString()}`,
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
+});
+phrase();
